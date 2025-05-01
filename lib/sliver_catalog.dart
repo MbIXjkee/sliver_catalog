eed5196ac693e002f635403381addb4d1df0bebf
Mikhail Zotyev <mbixjkee1392@gmail.com>
2025-05-01 20:59:07 +0200

    Feat: add export for LeavingViewportTransformRenderSliver in sliver_catalog.

--- lib/sliver_catalog.dart
+++ lib/sliver_catalog.dart
@@ -1,5 +1,6 @@
 library sliver_catalog;
 
 export 'package:sliver_catalog/src/base/leaving_viewport_shader_sliver.dart';
+export 'package:sliver_catalog/src/base/leaving_viewport_transform_render_sliver.dart';
 export 'package:sliver_catalog/src/glider.dart';
 export 'package:sliver_catalog/src/spinner.dart';
