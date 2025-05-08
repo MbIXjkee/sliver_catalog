d058ea6f5005966edf8d18c79173118706e07a23
Mikhail Zotyev <mbixjkee1392@gmail.com>
2025-05-08 16:51:09 +0200

    Feat: add blood sliver to the catalog.

---
+++ lib/src/blood.dart
@@ -0,0 +1,67 @@
+import 'dart:ui' as ui;
+
+import 'package:flutter/material.dart';
+import 'package:sliver_catalog/sliver_catalog.dart';
+
+/// A widget that applies a blood covering effect to the content of a sliver
+/// using a predefined shader effect.
+///
+/// See also:
+/// [LeavingViewportShaderSliver] a base for this sliver, that applies a passed
+/// shader to the child during the leaving of the visual part of the viewport.
+class BloodSliver extends StatefulWidget {
+  final Widget child;
+
+  const BloodSliver({
+    super.key,
+    required this.child,
+  });
+
+  @override
+  State<BloodSliver> createState() => _BloodSliverState();
+}
+
+class _BloodSliverState extends State<BloodSliver> {
+  late final Future<ui.FragmentShader?> _shaderFuture;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // False alarm - we just init a future for future builder.
+    // ignore: discarded_futures
+    _shaderFuture = _initShader();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<ui.FragmentShader?>(
+      future: _shaderFuture,
+      builder: (context, snapshot) {
+        final shader = snapshot.data;
+
+        return LeavingViewportShaderSliver(
+          shader: shader,
+          child: widget.child,
+        );
+      },
+    );
+  }
+
+  Future<ui.FragmentShader?> _initShader() async {
+    try {
+      final program = await ui.FragmentProgram.fromAsset(
+        'packages/sliver_catalog/assets/shaders/blood.frag',
+      );
+      final shader = program.fragmentShader();
+      
+      return shader;
+
+      // Any problem but not specific leads to failure using the shader.
+      // ignore: avoid_catches_without_on_clauses
+    } catch (e) {
+      debugPrint('Failed to init blood shader: $e');
+      return null;
+    }
+  }
+}
