8ed8b0c02c0b7e04996edd0049e5f73773640fdb
Mikhail Zotyev <mbixjkee1392@gmail.com>
2025-05-14 16:55:27 +0200

    Feat: add hit testing tests for LeavingViewportTransformedRenderSliver.

--- test/base/leaving_viewport_transform_render_sliver_test.dart
+++ test/base/leaving_viewport_transform_render_sliver_test.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: lines_longer_than_80_chars
+
 import 'package:flutter/rendering.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:sliver_catalog/sliver_catalog.dart';
@@ -91,6 +93,103 @@ void main() {
         expect(transformLayer.transform, equals(sliver.paintTransform));
       });
     });
+
+    group('hitTest', () {
+      late _HitTestSliver sliver;
+
+      setUp(() {
+        sliver = _HitTestSliver()..child = child;
+      });
+
+      group('should return value from specific implementation', () {
+        const testCases = [
+          true,
+          false,
+        ];
+        for (final testValue in testCases) {
+          test('case $testValue', () {
+            sliver.setTestHit(testValue: testValue);
+
+            final result = SliverHitTestResult();
+            final hit = sliver.hitTestChildren(
+              result,
+              mainAxisPosition: 0.0,
+              crossAxisPosition: 0.0,
+            );
+
+            expect(hit, equals(testValue));
+          });
+        }
+      });
+
+      test(
+        'should delegate adjusted hit point to child if specific implementation is not defined',
+        () {
+          final hitChild = _HitTestBox();
+          sliver
+            ..child = hitChild
+            ..setTestPaintTransform(Matrix4.diagonal3Values(0.5, 0.5, 1.0))
+            ..setTestHit(testValue: null);
+
+          final result = SliverHitTestResult();
+          sliver.hitTestChildren(
+            result,
+            mainAxisPosition: 0.0,
+            crossAxisPosition: 0.0,
+          );
+
+          expect(hitChild.triedPosition, isNotNull);
+        },
+      );
+
+      test(
+        'should correctly adjust hit test point for vertical orientation',
+        () {
+          final hitChild = _HitTestBox();
+          sliver
+            ..child = hitChild
+            ..setTestOrientation(isVertical: true)
+            ..setTestPaintTransform(Matrix4.diagonal3Values(0.5, 0.5, 1.0))
+            ..setTestHit(testValue: null);
+
+          final result = SliverHitTestResult();
+          sliver.hitTestChildren(
+            result,
+            mainAxisPosition: 10.0,
+            crossAxisPosition: 15.0,
+          );
+
+          final childHit = hitChild.triedPosition;
+          expect(childHit, isNotNull);
+          expect(childHit!.dx, equals(30.0));
+          expect(childHit.dy, equals(20.0));
+        },
+      );
+
+      test(
+        'should correctly adjust hit test point for horizontal orientation',
+        () {
+          final hitChild = _HitTestBox();
+          sliver
+            ..child = hitChild
+            ..setTestOrientation(isVertical: false)
+            ..setTestPaintTransform(Matrix4.diagonal3Values(0.5, 0.5, 1.0))
+            ..setTestHit(testValue: null);
+
+          final result = SliverHitTestResult();
+          sliver.hitTestChildren(
+            result,
+            mainAxisPosition: 10.0,
+            crossAxisPosition: 15.0,
+          );
+
+          final childHit = hitChild.triedPosition;
+          expect(childHit, isNotNull);
+          expect(childHit!.dx, equals(20.0));
+          expect(childHit.dy, equals(30.0));
+        },
+      );
+    });
   });
 }
 
@@ -118,4 +217,81 @@ class _PaintTestSliver extends _TestLayoutSliver {
   SliverGeometry? get geometry => const SliverGeometry(
         visible: true,
       );
+}
+
+class _HitTestSliver extends _TestLayoutSliver {
+  static const _verticalConstraints = SliverConstraints(
+    axisDirection: AxisDirection.down,
+    growthDirection: GrowthDirection.forward,
+    userScrollDirection: ScrollDirection.idle,
+    scrollOffset: 0.0,
+    precedingScrollExtent: 0.0,
+    overlap: 0.0,
+    remainingPaintExtent: 200.0,
+    crossAxisExtent: 300.0,
+    crossAxisDirection: AxisDirection.right,
+    viewportMainAxisExtent: 200.0,
+    remainingCacheExtent: 0.0,
+    cacheOrigin: 0.0,
+  );
+  static const _horizontalConstraints = SliverConstraints(
+    axisDirection: AxisDirection.right,
+    growthDirection: GrowthDirection.forward,
+    userScrollDirection: ScrollDirection.idle,
+    scrollOffset: 0.0,
+    precedingScrollExtent: 0.0,
+    overlap: 0.0,
+    remainingPaintExtent: 200.0,
+    crossAxisExtent: 300.0,
+    crossAxisDirection: AxisDirection.down,
+    viewportMainAxisExtent: 200.0,
+    remainingCacheExtent: 0.0,
+    cacheOrigin: 0.0,
+  );
+
+  bool? hasHit;
+  bool vertical = true;
+
+  // ignore: use_setters_to_change_properties
+  void setTestHit({required bool? testValue}) {
+    hasHit = testValue;
+  }
+
+  // ignore: use_setters_to_change_properties
+  void setTestOrientation({required bool isVertical}) {
+    vertical = isVertical;
+  }
+
+  @override
+  bool? performSpecificHitTestChildren(
+    SliverHitTestResult result, {
+    required double mainAxisPosition,
+    required double crossAxisPosition,
+  }) {
+    return hasHit;
+  }
+
+  @override
+  SliverGeometry? get geometry => const SliverGeometry(
+        hitTestExtent: 100,
+      );
+
+  @override
+  SliverConstraints get constraints =>
+      vertical ? _verticalConstraints : _horizontalConstraints;
+}
+
+class _HitTestBox extends RenderBox {
+  Offset? triedPosition;
+
+  @override
+  bool hitTest(BoxHitTestResult result, {required Offset position}) {
+    triedPosition = position;
+    return false;
+  }
+
+  @override
+  void performLayout() {
+    size = constraints.biggest;
+  }
 }
