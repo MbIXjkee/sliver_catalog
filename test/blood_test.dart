3330a0c07139b3e2293f0f6ab734c88af324faf6
Mikhail Zotyev <mbixjkee1392@gmail.com>
2025-05-16 16:30:54 +0200

    Refactor: join tests into group to follow the same structure as in other test files.

--- test/blood_test.dart
+++ test/blood_test.dart
@@ -6,46 +6,14 @@ import 'package:sliver_catalog/sliver_catalog.dart';
 import 'package:sliver_catalog/src/utils/shader_storage.dart';
 
 void main() {
-  testWidgets('applies shader when it is loaded', (tester) async {
-    final programLoading =
-        ui.FragmentProgram.fromAsset('assets/shaders/template.frag');
-
-    ShaderStorage.instance.setProgramForTest(
-      SupportedShaders.blood,
-      programLoading,
-    );
-
-    await tester.pumpWidget(
-      const MaterialApp(
-        home: CustomScrollView(
-          slivers: [
-            BloodSliver(
-              child: SizedBox(height: 100),
-            ),
-          ],
-        ),
-      ),
-    );
-
-    await programLoading;
-    await tester.pump(const Duration(seconds: 1));
-
-    final sliverFinder = find.byType(LeavingViewportShaderSliver);
-    expect(sliverFinder, findsOneWidget);
-    final sliverWidget =
-        tester.widget<LeavingViewportShaderSliver>(sliverFinder);
-    expect(sliverWidget.shader, isNotNull);
-  });
-
-  testWidgets(
-    'should work without crashing if shader fails to load',
-    (tester) async {
-      final failingFuture = Future<ui.FragmentProgram>.error(Exception(test))
-        ..ignore();
+  group('BloodSliver', () {
+    testWidgets('applies shader when it is loaded', (tester) async {
+      final programLoading =
+          ui.FragmentProgram.fromAsset('assets/shaders/template.frag');
 
       ShaderStorage.instance.setProgramForTest(
         SupportedShaders.blood,
-        failingFuture,
+        programLoading,
       );
 
       await tester.pumpWidget(
@@ -53,23 +21,57 @@ void main() {
           home: CustomScrollView(
             slivers: [
               BloodSliver(
-                child: SizedBox(
-                  height: 100,
-                ),
+                child: SizedBox(height: 100),
               ),
             ],
           ),
         ),
       );
 
+      await programLoading;
       await tester.pump(const Duration(seconds: 1));
 
-      expect(tester.takeException(), isNull);
       final sliverFinder = find.byType(LeavingViewportShaderSliver);
       expect(sliverFinder, findsOneWidget);
       final sliverWidget =
           tester.widget<LeavingViewportShaderSliver>(sliverFinder);
-      expect(sliverWidget.shader, isNull);
-    },
-  );
+      expect(sliverWidget.shader, isNotNull);
+    });
+
+    testWidgets(
+      'should work without crashing if shader fails to load',
+      (tester) async {
+        final failingFuture = Future<ui.FragmentProgram>.error(Exception(test))
+          ..ignore();
+
+        ShaderStorage.instance.setProgramForTest(
+          SupportedShaders.blood,
+          failingFuture,
+        );
+
+        await tester.pumpWidget(
+          const MaterialApp(
+            home: CustomScrollView(
+              slivers: [
+                BloodSliver(
+                  child: SizedBox(
+                    height: 100,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+
+        await tester.pump(const Duration(seconds: 1));
+
+        expect(tester.takeException(), isNull);
+        final sliverFinder = find.byType(LeavingViewportShaderSliver);
+        expect(sliverFinder, findsOneWidget);
+        final sliverWidget =
+            tester.widget<LeavingViewportShaderSliver>(sliverFinder);
+        expect(sliverWidget.shader, isNull);
+      },
+    );
+  });
 }
